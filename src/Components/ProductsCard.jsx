1aa8a8c353c57a5c27136a2f3249b9a67546e2b4
Joao-Ortega <joao_ortega17@hotmail.com>
2022-06-28 08:42:56 -0300

    Adding LINK tag to products

--- src/Components/ProductsCard.jsx
+++ src/Components/ProductsCard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 export default class ProductsCard extends Component {
   render() {
@@ -10,15 +11,19 @@ export default class ProductsCard extends Component {
          inStock
           ?
           <div className="product-card" key={ id }>
-            <img className="products-images" src={ gallery[0] } alt="" />
-            <p className="product-name">{ name }</p>
-            <p>{ `${prices[0].currency.symbol}${prices[0].amount}` }</p>
+            <Link to={`/${id}`} style={{ textDecoration: "none", color: "#1d1f22" }}>
+              <img className="products-images" src={ gallery[0] } alt="" />
+              <p className="product-name">{ name }</p>
+              <p>{ `${prices[0].currency.symbol}${prices[0].amount}` }</p>
+            </Link>
           </div> :
           <div className="product-card-out" key={ id }>
-            <p className="out-title">OUT OF STOCK</p>
-            <img className="out-image" src={ gallery[0] } alt="" />
-            <p className="product-name-out">{ name }</p>
-            <p className="price-out">{ `${prices[0].currency.symbol}${prices[0].amount}` }</p>
+            <Link to={`/${id}`} style={{ textDecoration: "none" }}>
+              <p className="out-title">OUT OF STOCK</p>
+              <img className="out-image" src={ gallery[0] } alt="" />
+              <p className="product-name-out">{ name }</p>
+              <p className="price-out">{ `${prices[0].currency.symbol}${prices[0].amount}` }</p>
+            </Link>
           </div>
        )) }
       </div>
